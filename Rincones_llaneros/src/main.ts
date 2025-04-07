fb46af51f793138a5523b249e60413c84850610b
Franckk24 <franck_ordonez@soy.sena.edu.co>
2025-04-07 10:18:13 -0500

    login y register

--- Rincones_llaneros/src/main.ts
+++ Rincones_llaneros/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { LoginComponent } from './app/pages/login/login.component';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { RegisterComponent } from './app/pages/register/register.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent,{
+    providers:[
+        provideRouter([
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent }
+          ]),
+        provideAnimations(),
+        provideHttpClient()
+    ]
+}).catch(err => console.error(err));
