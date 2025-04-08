4497185d60831a729a738bd0f0eed2d290034867
leojose04 <leojose04@gmail.com>
2025-04-08 17:20:39 -0500

    vista principal

--- Rincones_llaneros/src/main.ts
+++ Rincones_llaneros/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { LoginComponent } from './app/pages/login/login.component';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent,{
+    providers:[
+        provideRouter([
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+          ]),
+        provideHttpClient()
+    ]
+}).catch(err => console.error(err));
