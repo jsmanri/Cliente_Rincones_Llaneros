bec879bf09f7e3c89dca75f9c471678fcf0419e6
jsmanri <jsmanriquemesa14@gmail.com>
2025-04-10 17:17:44 -0500

    Creacion header sin inicio de sesion

--- Rincones_llaneros/src/app/app.routes.ts
+++ Rincones_llaneros/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeaderregistroComponent } from './pages/component/headerregistro/headerregistro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'headerregistro', pathMatch: 'full' },
+    { path: 'headerregistro', component: HeaderregistroComponent },
+];
--- Rincones_llaneros/src/main.ts
+++ Rincones_llaneros/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HeaderregistroComponent } from './app/pages/component/headerregistro/headerregistro.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent,{
+  providers:[
+    provideRouter([
+      { path: '', redirectTo: 'headerregistro', pathMatch: 'full' },
+      { path: 'headerregistro', component: HeaderregistroComponent },
+    ])
+  ]
+})
+  .catch(err => console.error(err));
