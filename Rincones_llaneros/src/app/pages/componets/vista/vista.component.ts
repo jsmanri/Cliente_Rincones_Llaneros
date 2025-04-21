41d6fe6dc55ee653445c6414af3755cc49080c10
leojose04 <leojose04@gmail.com>
2025-04-21 17:04:18 -0500

    sgd

--- Rincones_llaneros/src/app/pages/componets/vista/vista.component.ts
+++ Rincones_llaneros/src/app/pages/componets/vista/vista.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vista',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './vista.component.html',
   styleUrl: './vista.component.css'
 })
@@ -13,6 +16,27 @@ export class VistaComponent {
   goToPush(){
     this.router.navigate(['/vista2'])
   }
+
   
+  
+   // Array de imágenes para el carrusel
+   images: string[] = [
+    '/atardecer.jpg',
+    '/Casanare-3.jpg',
+    '/comida.jpg',
+    '/elemento.jpg'
+  ];
+
+  currentIndex: number = 0;
+
+  // Función para mover al siguiente elemento
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  // Función para mover al anterior elemento
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
 
--- Rincones_llaneros/src/app/pages/componets/vista2/vista2.component.ts
+++ Rincones_llaneros/src/app/pages/componets/vista2/vista2.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vista2',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './vista2.component.html',
   styleUrl: './vista2.component.css'
 })
 export class Vista2Component {
+  images: string[] = [
+    '/atardecer.jpg',
+    '/Casanare-3.jpg',
+    '/comida.jpg',
+    '/elemento.jpg'
+  ];
 
+  currentIndex: number = 0;
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  // Función para mover al anterior elemento
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
