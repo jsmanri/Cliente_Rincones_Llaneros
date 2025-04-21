6789cabdb4091513155c8f45814a2fa2e1da12f5
jsmanri <jsmanriquemesa14@gmail.com>
2025-04-21 17:05:50 -0500

    Arreglo de header sin registro

--- Rincones_llaneros/src/main.ts
+++ Rincones_llaneros/src/main.ts
@@ -7,8 +7,8 @@ import { provideRouter } from '@angular/router';
 bootstrapApplication(AppComponent,{
   providers:[
     provideRouter([
-      { path: '', redirectTo: 'headerregistro', pathMatch: 'full' },
-      { path: 'headerregistro', component: HeaderregistroComponent },
+      { path: '', redirectTo: 'headersinregistro', pathMatch: 'full' },
+      { path: 'headersinregistro', component: HeadersinregistroComponent },
     ])
   ]
 })
