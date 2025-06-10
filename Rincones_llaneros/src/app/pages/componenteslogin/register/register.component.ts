fa037bf26497acfb13cfd918e6ffb964f4a8969f
Franckk24 <Franck5.5@hotmail.com>
2025-06-10 17:11:10 -0500

    admin terminado

--- Rincones_llaneros/src/app/pages/componenteslogin/register/register.component.ts
+++ Rincones_llaneros/src/app/pages/componenteslogin/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -16,7 +21,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIcon } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { ViewChild,TemplateRef } from '@angular/core';
+import { ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -34,23 +39,22 @@ import { ViewChild,TemplateRef } from '@angular/core';
     MatOptionModule,
     MatCheckboxModule,
     MatIcon,
-    MatDialogModule
+    MatDialogModule,
   ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
   fotoPerfil: File | null = null;
   previewUrl: string | null = null;
   base64Image: string | null = null;
-  roles: { Id: number, Nombre: string }[] = [];
+  roles: { Id: number; Nombre: string }[] = [];
   registrationSuccess = false; // Indicador de registro exitoso
   showPassword = false;
   showConfirmPassword = false;
 
   @ViewChild('successDialog') successDialog!: TemplateRef<any>;
-
 
   constructor(
     private fb: FormBuilder,
@@ -58,58 +62,68 @@ export class RegisterComponent {
     private apiService: ApiService,
     private dialog: MatDialog
   ) {
-    this.registerForm = this.fb.group({
-      role: ['', Validators.required],
-      fullName: ['', [Validators.required, Validators.minLength(4)]],
-      fotoPerfil: [null, Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      cedula: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
-      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
-      password: ['', [
-        Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(10),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d$@$!%*?&]{8,}$')
-      ]],
-      confirmPassword: ['', Validators.required],
-      acceptTerms: [false, Validators.requiredTrue] // Aceptar términos es obligatorio
-    }, { validator: this.passwordMatchValidator });
+    this.registerForm = this.fb.group(
+      {
+        role: ['', Validators.required],
+        fullName: ['', [Validators.required, Validators.minLength(4)]],
+        fotoPerfil: [null, Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        cedula: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+        telefono: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]{10}$')],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(10),
+            Validators.pattern(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d$@$!%*?&]{8,}$'
+            ),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue], // Aceptar términos es obligatorio
+      },
+      { validator: this.passwordMatchValidator }
+    );
 
     this.loadRoles();
   }
 
   onlyNumberKey(event: KeyboardEvent): boolean {
-  const charCode = event.which ? event.which : event.keyCode;
-  // Permite solo números del 0 al 9 (códigos ASCII)
-  if (charCode < 48 || charCode > 57) {
-    event.preventDefault();
-    return false;
-  }
-  return true;
+    const charCode = event.which ? event.which : event.keyCode;
+    // Permite solo números del 0 al 9 (códigos ASCII)
+    if (charCode < 48 || charCode > 57) {
+      event.preventDefault();
+      return false;
+    }
+    return true;
   }
 
   getPasswordError(): string | null {
-  const control = this.registerForm.get('password');
-  const value = control?.value || '';
+    const control = this.registerForm.get('password');
+    const value = control?.value || '';
 
-  if (control?.hasError('required')) {
-    return 'La contraseña es obligatoria.';
-  }
-  if (!/[A-Z]/.test(value)) {
-    return 'Debe contener al menos una letra mayúscula.';
-  }
-  if (!/\d/.test(value)) {
-    return 'Debe contener al menos un número.';
-  }
-  if (!/[$@$!%*?&]/.test(value)) {
-    return 'Debe contener al menos un carácter especial.';
-  }
+    if (control?.hasError('required')) {
+      return 'La contraseña es obligatoria.';
+    }
+    if (!/[A-Z]/.test(value)) {
+      return 'Debe contener al menos una letra mayúscula.';
+    }
+    if (!/\d/.test(value)) {
+      return 'Debe contener al menos un número.';
+    }
+    if (!/[$@$!%*?&]/.test(value)) {
+      return 'Debe contener al menos un carácter especial.';
+    }
     if (value.length < 10) {
-    return 'La contraseña debe tener al menos 10 caracteres.';
+      return 'La contraseña debe tener al menos 10 caracteres.';
+    }
+    return null;
   }
-  return null;
-  }
-
 
   private loadRoles(): void {
     this.apiService.get<any>(API_URLS.CRUD.Api_crudRol).subscribe({
@@ -123,7 +137,7 @@ export class RegisterComponent {
       error: (err) => {
         console.error('Error al cargar roles:', err);
         alert('Error al cargar roles. Por favor, inténtalo nuevamente.');
-      }
+      },
     });
   }
 
@@ -162,8 +176,10 @@ export class RegisterComponent {
 
   register(): void {
     if (this.registerForm.invalid) {
-      Object.values(this.registerForm.controls).forEach(control => control.markAsTouched());
-   
+      Object.values(this.registerForm.controls).forEach((control) =>
+        control.markAsTouched()
+      );
+
       return;
     }
 
@@ -174,26 +190,29 @@ export class RegisterComponent {
       cedula: this.registerForm.value.cedula,
       telefono: this.registerForm.value.telefono,
       contrasena: this.registerForm.value.password,
-      fotoPerfil: this.base64Image || null
+      fotoPerfil: this.base64Image || null,
     };
-    console.log("json para el api mid ",usuario)
+    console.log('json para el api mid ', usuario);
 
-    this.apiService.post(API_URLS.Mid.Api_midCreacionUsuario, usuario).subscribe({
-      next: () => {
-        const dialogRef = this.dialog.open(this.successDialog, {
-          disableClose: true,
-          width: '350px'
-        });
-        setTimeout(() => {
-          dialogRef.close();
-          this.router.navigate(['/login']);
-        }, 3000);
-      },
-      error: (err) => {
-        console.error('Error al registrar usuario:', err);
-        alert('Error al registrar usuario. Por favor, inténtalo nuevamente.');
-      }
-    });
+    this.apiService
+      .post(API_URLS.Mid.Api_midCreacionUsuario, usuario)
+      .subscribe({
+        next: () => {
+          (document.activeElement as HTMLElement)?.blur();
+          const dialogRef = this.dialog.open(this.successDialog, {
+            disableClose: true,
+            width: '350px',
+          });
+          setTimeout(() => {
+            dialogRef.close();
+            this.router.navigate(['/login']);
+          }, 3000);
+        },
+        error: (err) => {
+          console.error('Error al registrar usuario:', err);
+          alert('Error al registrar usuario. Por favor, inténtalo nuevamente.');
+        },
+      });
   }
 
   goToPolitica() {
